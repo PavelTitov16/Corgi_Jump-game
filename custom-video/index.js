5fe038279cf79f67a07c13b86a3e7419f3883584
PavelTitov16 <pashoktitov16@gmail.com>
2022-02-05 23:45:09 +0300

    feat: add volume mute & volume input scroll

--- custom-video/index.js
+++ custom-video/index.js
@@ -3,10 +3,13 @@ const video = player.querySelector('.video');
 const button = document.querySelector('.button_3');
 const screenSaver = player.querySelector('.poster');
 const btnPlay = player.querySelector('.play_button');
-const muteBtn = player.querySelector('.volume-button');
-const volumeScale = document.getElementById('.video-hud__volume');
+const muteBtn = player.querySelector('.volume_button');
+const volume = document.querySelector('.volume');
+const progress = document.querySelector('.progress');
+let lastVideoVolume = volume.value;
+let currentTime = video.duration;
 
-function togglePlay () {
+function togglePlay() {
     if (video.paused) {
         video.play();
         screenSaver.classList.add('active');
@@ -21,17 +24,46 @@ function togglePlay () {
     }
 }
 
-function videoMute() { 
-    if(player.volume == 0) {
-    player.volume = volumeScale.value / 100;
-    muteButton.classList.remove('mute');
-    muteButton.classList.add('volume_on');
+volume.addEventListener('input', function () {
+    const value = this.value;
+    video.volume = value;
+    lastVideoVolume = value;
+    if (value == 0) {
+        onVideoMute();
     } else {
-    player.volume = 0;
-    muteButton.classList.remove('volume_on');
-    muteButton.classList.add('mute');
+        onVideoVolume();
+    }
+    this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #c8c8c8 ${value}%, #c8c8c8 100%)`;
+})
+
+function onVideoMute() {
+    muteBtn.classList.remove('volume_on');
+    muteBtn.classList.add('mute');
+}
+function onVideoVolume() {
+    muteBtn.classList.remove('mute');
+    muteBtn.classList.add('volume_on');
+}
+function videoMute() {
+    if (video.volume == 0) {
+        video.volume = lastVideoVolume;
+        volume.value = lastVideoVolume;
+        muteBtn.classList.remove('mute');
+        muteBtn.classList.add('volume_on');
+    } else {
+        video.volume = 0;
+        muteBtn.classList.remove('volume_on');
+        muteBtn.classList.add('mute');
+        volume.value = 0;
     }
 }
+
+progress.addEventListener('input', function () {
+    const value = this.value;
+    video.currentTime = value * video.duration;
+    console.log(video.currentTime);
+    this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #c8c8c8 ${value}%, #c8c8c8 100%)`;
+})
 
 button.addEventListener('click', togglePlay);
 btnPlay.addEventListener('click', togglePlay);
