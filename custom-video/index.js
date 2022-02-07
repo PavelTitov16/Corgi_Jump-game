bdd68d123bcf3d98edcd724be4cc79a22a2f24df
PavelTitov16 <pashoktitov16@gmail.com>
2022-02-07 22:48:57 +0300

    feat: add fullcreen & progress functions

--- custom-video/index.js
+++ custom-video/index.js
@@ -6,35 +6,68 @@ const btnPlay = player.querySelector('.play_button');
 const muteBtn = player.querySelector('.volume_button');
 const volume = document.querySelector('.volume');
 const progress = document.querySelector('.progress');
+const fullScreenBtn = document.querySelector('.fullscreen');
 let lastVideoVolume = volume.value;
-let currentTime = video.duration;
+let progressId = null;
+let wasPlaying = false;
 
+window.addEventListener('load', function () {
+    progress.max = video.duration;
+})
+
+/* Play - Pause */
 function togglePlay() {
     if (video.paused) {
         video.play();
+        progressId = setInterval(changeProgress, 100);
         screenSaver.classList.add('active');
         button.classList.toggle('active');
         btnPlay.classList.remove('play');
         btnPlay.classList.add('pause');
     } else {
         video.pause();
+        clearInterval(progressId);
         button.classList.remove('active');
         btnPlay.classList.remove('pause');
         btnPlay.classList.add('play');
     }
 }
 
-volume.addEventListener('input', function () {
-    const value = this.value;
-    video.volume = value;
-    lastVideoVolume = value;
-    if (value == 0) {
+button.addEventListener('click', togglePlay);
+btnPlay.addEventListener('click', togglePlay);
+
+video.addEventListener('ended', () => {
+    button.classList.toggle('active');
+    btnPlay.classList.remove('pause');
+    btnPlay.classList.add('play');
+    clearInterval(progressId);
+});
+/* Play - Pause */
+
+/* Volume - Mute */
+function changeVolume() {
+    volume.value = video.volume;
+    lastVideoVolume = volume.value;
+    changeVolumeColor();
+}
+
+volume.addEventListener('change', function () {
+    video.volume = volume.value;
+    lastVideoVolume = volume.value;
+    if (video.volume == 0) {
         onVideoMute();
     } else {
         onVideoVolume();
     }
-    this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #c8c8c8 ${value}%, #c8c8c8 100%)`;
 })
+
+function changeVolumeColor() {
+    const max = volume.max;
+    const value = (volume.value / max) * 100;
+    volume.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #fff ${value}%, #fff 100%)`;
+}
+
+volume.addEventListener('input', changeVolumeColor);
 
 function onVideoMute() {
     muteBtn.classList.remove('volume_on');
@@ -47,45 +80,63 @@ function onVideoVolume() {
 function videoMute() {
     if (video.volume == 0) {
         video.volume = lastVideoVolume;
-        volume.value = lastVideoVolume;
-        muteBtn.classList.remove('mute');
-        muteBtn.classList.add('volume_on');
+        volume.value = video.volume;
+        onVideoVolume();
+        changeVolumeColor()
     } else {
+        onVideoMute();
         video.volume = 0;
-        muteBtn.classList.remove('volume_on');
-        muteBtn.classList.add('mute');
         volume.value = 0;
+        changeVolumeColor()
     }
 }
 
-progress.addEventListener('input', function () {
-    const value = this.value;
-    video.currentTime = value * video.duration;
-    console.log(video.currentTime);
-    this.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #c8c8c8 ${value}%, #c8c8c8 100%)`;
-})
-
-button.addEventListener('click', togglePlay);
-btnPlay.addEventListener('click', togglePlay);
 muteBtn.addEventListener('click', videoMute);
+/* Volume - Mute */
 
-video.addEventListener('ended', () => { 
-    button.classList.toggle('active');
-    btnPlay.classList.remove('pause');
-    btnPlay.classList.add('play');
+/* Progress */
+function changeProgress() {
+    progress.value = video.currentTime;
+    changeProgressColor();
+}
+
+function changeProgressColor() {
+    const max = progress.max;
+    const value = (progress.value / max) * 100;
+    progress.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${value}%, #fff ${value}%, #fff 100%)`;
+}
+
+progress.addEventListener('input', changeProgressColor);
+
+progress.addEventListener('mousedown', function () {
+    wasPlaying = !video.paused;
+    if (wasPlaying) {
+        video.pause();
+        clearInterval(progressId);
+    }
 });
-/*function progressUpd {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexbasis = `${percent}%`; */
-/*onst progressTimeLine = (e) => {
-    const newTime = e.offsetX / progressWrapper.offsetWidth
-    progressBar.style.width = `${newTime * 100}%`
-    video.currentTime = newTime * video.duration
-  }
 
-  progressWrapper.addEventListener('click', progressTimeLine)*/
-/*
-range.addEventListener('change', rangeUpdate);
-range.addEventListener('mousemove', rangeUpdate);
-video.addEventListener('timeUpdate', rangeUpdate);
- */
+progress.addEventListener('change', function () {
+    video.currentTime = progress.value;
+    if (wasPlaying) {
+        video.play();
+        progressId = setInterval(changeProgress, 100);
+    } else {
+        changeProgress();
+    }
+});
+/* Progress */
+
+/* Fullscreen */
+const openFullscreen = () => {
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    }
+};
+fullScreenBtn.addEventListener('click', () => {
+    openFullscreen();
+});
+/* Fullscreen */
+
+
+
