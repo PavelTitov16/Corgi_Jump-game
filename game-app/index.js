138809737e583c16795dc7f2ba54819025479fdd
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-24 17:08:08 +0300

    fix repair bonuses spawn and jump keydown

--- game-app/index.js
+++ game-app/index.js
@@ -49,11 +49,11 @@ const backMusic = new Audio();
 backMusic.src = './assets/sounds/FMT_Game.mp3';
 backMusic.loop = true;
 
-const jumpSd = new Audio();
-jumpSd.src = './assets/sounds/jump!.mp3';
+const swipeSd = new Audio();
+swipeSd.src = './assets/sounds/jump!.mp3';
 
-const doubleJumpSd = new Audio();
-doubleJumpSd.src = './assets/sounds/jump.mp3';
+const JumpSd = new Audio();
+JumpSd.src = './assets/sounds/jump.mp3';
 
 const scoreSd = new Audio();
 scoreSd.src = './assets/sounds/score.mp3';
@@ -74,7 +74,7 @@ const state = {
     getReady: 0,
     gameOn: 1,
     gameOver: 2
-}
+};
 
 function stageSwitch() {
     switch (state.current) {
@@ -82,10 +82,11 @@ function stageSwitch() {
             state.current = state.gameOn;
             break;
         case state.gameOn:
+
             backMusic.play();
             break;
         case state.gameOver:
-            location.reload()
+            location.reload();
             state.current = state.getReady;
             break;
     }
@@ -93,12 +94,13 @@ function stageSwitch() {
 /* Game stages */
 
 /* Sky */
+
 class Background {
     constructor() {
         this.position = {
             x: 0,
             y: 0
-        }
+        };
 
         this.width = canvas.width;
         this.height = canvas.height;
@@ -116,6 +118,7 @@ class Background {
 }
 
 const bg = new Background();
+
 /* Sky */
 
 /* Get Ready */
@@ -124,7 +127,7 @@ class GetReady {
         this.position = {
             x: 300,
             y: game.height - 500
-        }
+        };
 
         this.width = 550;
         this.height = 400;
@@ -150,7 +153,7 @@ class GameOver {
         this.position = {
             x: 350,
             y: game.height - 580
-        }
+        };
 
         this.width = 512;
         this.height = 512;
@@ -176,12 +179,12 @@ class Foreground {
         this.position = {
             x: 0,
             y: 0
-        }
+        };
 
         this.velocity = {
-            x: -5,
+            x: -6,
             y: 0
-        }
+        };
 
         this.width = canvas.width;
         this.height = canvas.height;
@@ -200,19 +203,19 @@ class Foreground {
     }
 }
 
-const fg = [new Foreground(), new Foreground(), new Foreground()];
+const fg = [new Foreground(), new Foreground()];
 
 class Clouds {
     constructor() {
         this.position = {
             x: 0,
             y: 0
-        }
+        };
 
         this.velocity = {
-            x: -5,
+            x: -3,
             y: 0
-        }
+        };
 
         this.width = canvas.width;
         this.height = canvas.height;
@@ -231,7 +234,7 @@ class Clouds {
     }
 }
 
-const cld = [new Clouds(), new Clouds(), new Clouds()];
+const cld = [new Clouds(), new Clouds()];
 /* Field */
 
 /* Corgi */
@@ -242,22 +245,23 @@ class Corgi {
         this.position = {
             x: 100,
             y: 500
-        }
+        };
+
         this.velocity = {
             x: 0,
-            y: 1
-        }
+            y: 0
+        };
 
         this.width = 100;
         this.height = 100;
 
         this.image = corgiImage;
         this.image = jumpCorgi;
-        this.is_jump = false;
+
     }
 
     draw() {
-        if (this.velocity.y == 0) {
+        if (this.velocity.y === 0) {
             ctx.drawImage(corgiImage, this.position.x, this.position.y, this.width, this.height);
         } else ctx.drawImage(jumpCorgi, this.position.x, this.position.y, this.width, this.height);
     }
@@ -274,17 +278,78 @@ class Corgi {
 const corgi = new Corgi();
 /* Corgi */
 
-/* Sheep */
+/* Movement */
+const keys = {
+    jump: {
+        pressed: false
+    },
+    swipe: {
+        pressed: false
+    }
+}
+
+document.addEventListener('keydown', function (event) {
+    keyCode = event.keyCode;
+
+    if (keyCode === 13) {
+        stageSwitch();
+    }
+    if (state.current == state.gameOn) {
+
+        switch (keyCode) {
+            case 83:
+                if (keys.swipe.pressed) {
+                    return;
+                }
+                if (!keys.swipe.pressed && corgi.velocity.y === 0) {
+                    setTimeout(function () {
+                        corgi.velocity.y += 1;
+                        swipeSd.play();
+                    }, 2000);
+                    keys.swipe.pressed = true;
+                }else corgi.velocity.y = 0;
+                break;
+            case 87:
+                if (keys.jump.pressed) {
+                    return;
+                }
+                if (!keys.jump.pressed && corgi.velocity.y === 0) {
+                    corgi.velocity.y = -30;
+                    keys.jump.pressed = true;
+                    JumpSd.play();
+                } else corgi.velocity.y += gravity;
+                break;
+        }
+    }
+
+});
+
+document.addEventListener('keyup', ({
+    keyCode
+}) => {
+    switch (keyCode) {
+        case 83:
+            keys.swipe.pressed = false;
+            break;
+        case 87:
+            keys.jump.pressed = false;
+            break;
+    }
+});
+/* Movement */
+
+/* Sheeps */
 class Sheep {
     constructor() {
         this.position = {
             x: 1200,
             y: 500
-        }
+        };
+
         this.velocity = {
             x: -12,
             y: 0
-        }
+        };
 
         this.width = 110;
         this.height = 110;
@@ -302,17 +367,20 @@ class Sheep {
     }
 }
 const sheeps = [];
+
 setInterval(function () {
-    const randomValue = getRandomValue(0, 100);
-    if (randomValue > 65) {
-        sheeps.push(new Sheep())
+    if (state.current === 1) {
+        const randomValue = getRandomValue(0, 100);
+        if (randomValue > 65) {
+            sheeps.push(new Sheep())
+        }
     }
 }, 900);
 
 function getRandomValue(min, max) {
     return Math.random() * (max - min) + min;
 }
-/* Sheep */
+/* Sheeps */
 
 /* Score */
 class Score {
@@ -364,18 +432,18 @@ let score = new Score;
 class Bone {
     constructor() {
         this.position = {
-            x: 1200,
-            y: getRandomValue(200, 400)
-        }
-        this.velocity = {
-            x: -13,
-            y: 0
-        }
+                x: 1200,
+                y: getRandomValue(200, 400)
+            },
+            this.velocity = {
+                x: -13,
+                y: 0
+            },
 
-        this.width = 80;
-        this.height = 80;
+            this.width = 80,
+            this.height = 80,
 
-        this.image = boneImage;
+            this.image = boneImage;
     }
 
     draw() {
@@ -388,9 +456,13 @@ class Bone {
     }
 }
 const bones = [];
+
 setInterval(function () {
-    bones.push(new Bone())
+    if (state.current === 1) {
+        bones.push(new Bone());
+    }
 }, 10000);
+
 
 setInterval(function () {
     bones.forEach(bone => {
@@ -401,26 +473,26 @@ setInterval(function () {
             bonusSd.play();
             score.bonus();
         }
-    })
-}, 100)
+    });
+}, 100);
 /* Bone */
 
 /* Donut */
 class Donut {
     constructor() {
         this.position = {
-            x: 1200,
-            y: getRandomValue(200, 400)
-        }
-        this.velocity = {
-            x: -16,
-            y: 0
-        }
+                x: 1200,
+                y: getRandomValue(200, 400)
+            },
+            this.velocity = {
+                x: -16,
+                y: 0
+            },
 
-        this.width = 80;
-        this.height = 80;
+            this.width = 80,
+            this.height = 80,
 
-        this.image = donutImage;
+            this.image = donutImage;
     }
 
     draw() {
@@ -433,9 +505,14 @@ class Donut {
     }
 }
 const donuts = [];
+
 setInterval(function () {
-    donuts.push(new Donut())
+    if (state.current === 1) {
+        donuts.push(new Donut())
+    }
 }, 20000);
+
+
 
 setInterval(function () {
     donuts.forEach(donut => {
@@ -447,25 +524,25 @@ setInterval(function () {
             score.high_bonus();
         }
     });
-}, 100)
+}, 100);
 /* Donut */
 
 /* Fly Coat */
 class Coat {
     constructor() {
         this.position = {
-            x: 1200,
-            y: getRandomValue(200, 350)
-        }
-        this.velocity = {
-            x: -18,
-            y: 0
-        }
+                x: 1200,
+                y: getRandomValue(200, 350)
+            },
+            this.velocity = {
+                x: -18,
+                y: 0
+            },
 
-        this.width = 70;
-        this.height = 70;
+            this.width = 70,
+            this.height = 70,
 
-        this.image = coatImage;
+            this.image = coatImage;
     }
 
     draw() {
@@ -478,10 +555,12 @@ class Coat {
     }
 }
 const coats = [];
-setInterval(function () {
-    coats.push(new Coat());
-}, 35000);
 
+setInterval(function () {
+    if (state.current === 1) {
+        coats.push(new Coat());
+    }
+}, 35000);
 
 setInterval(function () {
     coats.forEach(coat => {
@@ -489,76 +568,21 @@ setInterval(function () {
             corgi.position.y + corgi.height - 30 >= coat.position.y && corgi.position.y - corgi.height + 30 <= coat.position.y + coat.height - 30) {
             console.log('bonus');
             coats.shift();
+            backMusic.pause();
             flySound.play();
             corgi.velocity.y = -20;
             gravity = 0;
-            keyS.jump.pressed = true;
-            keyS.double_jump.pressed = true;
+            keys.jump.pressed = true;
+            keys.swipe.pressed = true;
             setTimeout(function () {
-                corgi.velocity.y = 0;
                 gravity = 1.7;
+                corgi.velocity.y += gravity;
+                backMusic.play();
             }, 15000);
         }
     });
-}, 100)
+}, 100);
 /* Fly Coat */
-
-/* Animation */
-function animate() {
-    requestAnimationFrame(animate);
-    bg.update();
-    if (state.current === 0) {
-        getReady.update();
-    }
-    if (state.current === 1) {
-        fg.forEach((elem) => {
-            elem.update();
-            if (elem.position.x + elem.width <= 0) {
-                fg.push(new Foreground);
-                fg.shift();
-            }
-        });
-        cld.forEach((cloud) => {
-            cloud.update();
-            if (cloud.position.x + cloud.width <= 0) {
-                cld.push(new Clouds);
-                cld.shift();
-            }
-        });
-        corgi.update();
-        sheeps.forEach((sheep) => {
-            sheep.update();
-            if (sheep.position.x + sheep.width <= 0) {
-                score.count();
-                scoreSd.play();
-                sheeps.shift();
-            }
-        });
-        donuts.forEach((donut) => {
-            donut.update();
-            if (donut.position.x + donut.width <= 0) {
-                donuts.shift();
-            }
-        });
-        bones.forEach((bone) => {
-            bone.update();
-            if (bone.position.x + bone.width <= 0) {
-                bones.shift();
-            }
-        });
-        coats.forEach((coat) => {
-            coat.update();
-            if (coat.position.x + coat.width <= 0) {
-                coats.shift();
-            }
-        });
-        score.update();
-    }
-    if (state.current === 2) {
-        gameEnd.update();
-    }
-}
-/* Animation */
 
 /* Lose */
 setInterval(function () {
@@ -571,8 +595,8 @@ setInterval(function () {
             backMusic.pause();
             setTimeout(countLastResults, 2000);
         }
-    })
-}, 100)
+    });
+}, 100);
 /* Lose */
 
 /* Local Storage */
@@ -608,68 +632,67 @@ function countLastResults() {
 }
 /* Local Storage */
 
+/* Animation */
+function animate() {
+    requestAnimationFrame(animate);
 
-/* Movement */
-const keyS = {
-    jump: {
-        pressed: false
-    },
-    double_jump: {
-        pressed: false
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    bg.update();
+    if (state.current === 0) {
+        getReady.update();
+    }
+    if (state.current === 1) {
+        cld.forEach((cloud) => {
+            cloud.update();
+            if (cloud.position.x + cloud.width <= 0) {
+                cld.push(new Clouds);
+                cld.shift();
+            }
+        });
+        fg.forEach((elem) => {
+            elem.update();
+            if (elem.position.x + elem.width <= 0) {
+                fg.push(new Foreground);
+                fg.shift();
+            }
+        });
+        corgi.update();
+
+        sheeps.forEach((sheep) => {
+            sheep.update();
+            if (sheep.position.x + sheep.width <= 0) {
+                score.count();
+                scoreSd.play();
+                sheeps.shift();
+            }
+        });
+        donuts.forEach((donut) => {
+            donut.update();
+            if (donut.position.x + donut.width <= 0) {
+                donuts.shift();
+            }
+        });
+        bones.forEach((bone) => {
+            bone.update();
+            if (bone.position.x + bone.width <= 0) {
+                bones.shift();
+            }
+        });
+        coats.forEach((coat) => {
+            coat.update();
+            if (coat.position.x + coat.width <= 0) {
+                coats.shift();
+            }
+        });
+        score.update();
+    }
+    if (state.current === 2) {
+        gameEnd.update();
     }
 }
 
-document.addEventListener('keydown', function (event) {
-    keyCode = event.keyCode;
+/* Animation */
 
-    if (keyCode === 13) {
-        stageSwitch();
-    }
-    if (state.current == state.gameOn) {
-
-        switch (keyCode) {
-            case 81:
-                console.log('jump');
-                if (keyS.jump.pressed) {
-                    return;
-                }
-                if (!keyS.jump.pressed && corgi.velocity.y === 0) {
-                    corgi.velocity.y -= 20;
-                    keyS.jump.pressed = true;
-                    jumpSd.play();
-                } else corgi.velocity.y = 0;
-                break;
-            case 87:
-                console.log('double_jump');
-                if (keyS.double_jump.pressed) {
-                    return;
-                }
-                if (!keyS.double_jump.pressed && corgi.velocity.y === 0) {
-                    corgi.velocity.y -= 30;
-                    keyS.double_jump.pressed = true;
-                    doubleJumpSd.play();
-                } else corgi.velocity.y = 0;
-                break;
-        }
-    }
-
-});
-
-document.addEventListener('keyup', ({
-    keyCode
-}) => {
-    switch (keyCode) {
-        case 81:
-            corgi.velocity.y = 0;
-            keyS.jump.pressed = false;
-            break;
-        case 87:
-            corgi.velocity.y = 0;
-            keyS.double_jump.pressed = false;
-            break;
-    }
-});
-/* Movement */
 
 animate();
 
@@ -724,3 +747,102 @@ Stages */
         console.log(state.current);
     }
 }*/
+
+/* 
+
+class Background {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = canvas.width;
+        this.gameHeight = canvas.height;
+        this.image = backGround;
+        this.parallax1 = foreGround;
+        this.parallax1X = 0;
+        this.parallax2 = clouds;
+        this.parallax2X = 0;
+
+        this.position = {
+            x: 0,
+            y: 0
+        }
+        this.width = 1200;
+        this.height = 600;
+    
+    }
+    draw(context) {
+        ctx.drawImage(this.parallax2, this.parallax2X, this.y, this.width, this.height);
+        ctx.drawImage(this.parallax2, this.parallax2X + this.width, this.y, this.width, this.height);
+
+        ctx.drawImage(this.parallax1, this.parallax1X, this.y, this.width, this.height);
+        ctx.drawImage(this.parallax1, this.parallax1X + this.width, this.y, this.width, this.height);
+
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+
+    }
+    update() {
+        this.parallax1X -= 6;
+        this.parallax2X -= 3;
+
+        if (this.x < 0 - this.width) this.x = 0;
+        if (this.parallax1X < 0 - this.width) this.parallax1X = 0;
+        if (this.parallax2X < 0 - this.width) this.parallax2X = 0;
+        
+        this.draw(ctx);
+    }
+}
+
+const bg = new Background();
+
+
+
+ class InputHandler {
+    constructor() {
+      this.keys = [];
+      window.addEventListener('keydown', (e) => {
+        if ((e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') && this.keys.indexOf(e.key) === -1) {
+          this.keys.push(e.key);
+        }
+      });
+      window.addEventListener('keyup', (e) => {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      });
+    }
+  }
+
+
+    //controls
+      if (input.keys.indexOf('ArrowUp') > -1 && this.groundCheck()) {
+        this.vy = -20;
+      } else if (input.keys.indexOf('ArrowLeft') > -1) {
+        this.speed = -5;
+      } else if (input.keys.indexOf('ArrowRight') > -1) {
+        this.speed = 5;
+      } else {
+        this.speed = 0;
+      }
+
+      //horizontal move
+      this.x += this.speed;
+      if (this.x < 0) this.x = 0;
+      else if (this.x > canvas.width - this.width) this.x = canvas.width - this.width;
+
+      //vertical move
+      this.y += this.vy;
+      if (!this.groundCheck()) {
+        this.vy += this.gravity;
+        this.frameX = 18;
+      } else {
+        this.vy = 0;
+        // this.frameX = 0;
+      }
+
+      if (this.y > this.gameHeight - this.height) this.y = this.playerHeight;
+    }
+
+    groundCheck() {
+      return this.y >= this.playerHeight;
+    }
+  }
+*/
